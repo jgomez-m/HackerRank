3e91eef64a47d88ac04752f930f956a7c6789c5f
Julian Gomez <madknight@gmail.com>
2020-12-23 16:48:40 -0500

    Solution using streams

--- src/hacker/rank/mostfrequent/Solution.java
+++ src/hacker/rank/mostfrequent/Solution.java
@@ -7,10 +7,38 @@ public class Solution {
 
     public static void main(String[] args) {
         String input = "1, 3, -5, 3, 3, -5, 1, -5, -5, 0, 100, 20, 9, 0, 3, 0, 3, A, F";
-        Solution.getTheThreeMost(input);
+        Map<?,?> map1 = getTheThreeMost(input);
+        Map<?,?> map2 = getTheThreeMostStreams(input);
+        map1.forEach((key, value) -> System.out.println(key +"->"+ value));
+        map2.forEach((key, value) -> System.out.println(key +"->"+ value));
     }
 
-    public static void getTheThreeMost(String input) {
+    public static Map<?,?> getTheThreeMostStreams(String input){
+        String[] tokens = input.split(",");
+        Map<String, Long> mapSorted = Arrays.stream(tokens)
+                .map(String::trim)
+                .filter(Solution::isNumber)
+                .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(3)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        return mapSorted;
+    }
+
+    private static boolean isNumber(String str){
+        try{
+            Integer.valueOf(str);
+            return true;
+        }catch (NumberFormatException ignored){
+
+        }
+        return false;
+    }
+
+    public static Map<?,?> getTheThreeMost(String input) {
         // 1. Split String in an List
         // 2. Filter by Numbers
         // 3. Map count numbers
@@ -25,11 +53,6 @@ public class Solution {
                 numList.add(number);
                 mapCount.computeIfPresent(number, (k,v) -> v+1);
                 mapCount.putIfAbsent(number, 1);
-//                if(mapCount.containsKey(number)){
-//                    mapCount.put(number, mapCount.get(number)+1);
-//                } else{
-//                    mapCount.put(number, 1);
-//                }
             }catch (NumberFormatException e) {
                 //skip
             }
@@ -41,6 +64,6 @@ public class Solution {
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                         (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
-        mapSorted.forEach((key, value) -> System.out.println(key));
+        return mapSorted;
     }
 }
